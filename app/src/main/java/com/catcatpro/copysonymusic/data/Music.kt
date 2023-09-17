7491de4d2617d2cbc85add62e633bc9db650feaf
絶対にできる <oncwnuI7Guya1XRuNQskfpuM_4kk@git.weixin.qq.com>
2023-09-17 23:10:16 +0800

    更改音乐数据结构

--- app/src/main/java/com/catcatpro/copysonymusic/data/Music.kt
+++ app/src/main/java/com/catcatpro/copysonymusic/data/Music.kt
@@ -5,7 +5,7 @@ import androidx.annotation.DrawableRes
 
 /**
  * @param title 标题
- * @param albumImage 专辑图片
+ * @param musicImage 歌曲封面
  * @param artist 艺术家
  * @param album 专辑
  * @param albumArtist 专辑艺术家
@@ -14,4 +14,4 @@ import androidx.annotation.DrawableRes
  * @param year 发行年份
  * @param discNumber 光盘编号
  */
-data class Music(val title:String,val albumImage: Int, val artist:String?, val album:String?, val albumArtist:String = "", val composer: String = "", val genre: String = "", val year:String ="", val discNumber: Number = 0)
+data class Music(val title:String,val musicImage: Int, val artist:String?, val album:String?, val albumArtist:String = "", val composer: String = "", val genre: String = "", val year:String ="", val discNumber: Number = 0)
