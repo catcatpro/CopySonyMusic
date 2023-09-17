452587db760c51c910d7892d5eceb4caae257a22
絶対にできる <oncwnuI7Guya1XRuNQskfpuM_4kk@git.weixin.qq.com>
2023-09-17 23:09:37 +0800

    添加专辑数据结构

--- app/src/main/java/com/catcatpro/copysonymusic/data/Album.kt
+++ app/src/main/java/com/catcatpro/copysonymusic/data/Album.kt
@@ -1,3 +1,11 @@
 package com.catcatpro.copysonymusic.data
 
-data class Album()
+
+/**
+ * @param title 专辑标题
+ * @param albumImage 专辑图片
+ * @param artist 艺术家
+ * @param genre 风格
+ * @param year 发行年份
+ */
+data class Album(val title:String,val albumImage: Int, val artist:String?,  val genre: String = "", val year:String ="")
